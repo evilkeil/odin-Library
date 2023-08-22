2c598ebb2ed4fdbec45abeaacb8b8f590c9e9899
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-08-22 13:46:01 +0900

    find a way to cycle through the library and add a book title from the array

--- app.js
+++ app.js
@@ -15,8 +15,28 @@ addBookToLibrary(){
 }
 
 const reZero = new Book("Re Zero","Tappei Nagatsuki",30,true).addBookToLibrary();
-const overlord = new Book("Overlord","Kugane Maruyama",15,false).addBookToLibrary();
+// const overlord = new Book("Overlord","Kugane Maruyama",15,false).addBookToLibrary();
+
+
+const container = document.querySelector('.container');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click',function(){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('book');
+    container.appendChild(newDiv);
+    
+    const lastBookIndex = myLibrary.length-1;
+    const title = document.createElement('h1');
+    title.textContent = `${myLibrary[lastBookIndex].title}`;
+
+    newDiv.appendChild(title);
+    
+
+})
 
 
 
-console.log(myLibrary)
+
+// console.log(container)
+
