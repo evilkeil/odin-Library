b8216d708f91d6b1fa0bc913b227d1991d3c3f7b
evilkeil <114153280+evilkeil@users.noreply.github.com>
2023-08-22 12:33:00 +0900

    create function to add books to the library and add it to books prototype

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ function Book(title,author,volumes,read){
     this.author = author;
     this.volumes = volumes;
     this.read = read;
-
 }
+
+Book.prototype = {
+addBookToLibrary(){
+    myLibrary.push(this);
+    return this;
+}
+}
+
+const reZero = new Book("Re Zero","Tappei Nagatsuki",30,true).addBookToLibrary();
+const overlord = new Book("Overlord","Kugane Maruyama",15,false).addBookToLibrary();
+
+
+
+console.log(myLibrary)
